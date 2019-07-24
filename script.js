6d5707123894a24369607287435f1b7906488f8b
Doga Tekin <doga.tekin@epfl.ch>
2019-07-24 11:47:46 +0100

    Try to fix lag

--- script.js
+++ script.js
@@ -24,6 +24,27 @@ function showAnimation() {
     }, 7000)
 }
 
+var options = {
+    strings: ["am a computer scientist.",
+        "am a Master's student.",
+        "am interested in machine learning.",
+        "love Python.",
+        "can juggle 3 balls.",
+        "have a playing card collection.",
+        "used to make <a onclick='showAnimation()'>stick figure animations</a>.",
+        "spend too much time on YouTube.",
+        "suggest you watch <a href='https://www.imdb.com/title/tt5687612/' target='_blank'>Fleabag</a>.",
+        "wish you a pleasant day."
+    ],
+    startDelay: 250,
+    typeSpeed: 40,
+    loop: true,
+    autoInsertCss: false,
+    smartBackspace: true
+}
+
+var typed = new Typed(".typed", options);
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // Get all "navbar-burger" elements
@@ -47,26 +68,5 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     }
-
-    var options = {
-        strings: ["am a computer scientist.",
-            "am a Master's student.",
-            "am interested in machine learning.",
-            "love Python.",
-            "can juggle 3 balls.",
-            "have a playing card collection.",
-            "used to make <a onclick='showAnimation()'>stick figure animations</a>.",
-            "spend too much time on YouTube.",
-            "suggest you watch <a href='https://www.imdb.com/title/tt5687612/' target='_blank'>Fleabag</a>.",
-            "wish you a pleasant day."
-        ],
-        startDelay: 250,
-        typeSpeed: 40,
-        loop: true,
-        autoInsertCss: false,
-        smartBackspace: true
-    }
-
-    var typed = new Typed(".typed", options);
 
 });
