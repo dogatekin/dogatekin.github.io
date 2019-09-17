500025e2d84bbbac9964fb9481f7342f0d868eb6
Doga Tekin <doga.tekin@epfl.ch>
2019-09-18 00:57:50 +0100

    Refactor into multiple files

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("content-tab");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    // tablinks = document.getElementsByClassName("tab");
-    // for (i = 0; i < x.length; i++) {
-    //     tablinks[i].className = tablinks[i].className.replace(" is-active", "");
-    // }
-    document.getElementById(tabName).style.display = "block";
-    // evt.currentTarget.className += " is-active";
-
-    document.getElementById("navBurger").classList.remove('is-active')
-    document.getElementById("navMenu").classList.remove('is-active')
-    window.scrollTo(0, 0)
-}
-
 function showAnimation() {
     document.getElementById("animation").style.display = "inline";
     setTimeout(() => {
@@ -44,29 +26,3 @@ var options = {
 }
 
 var typed = new Typed(".typed", options);
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-
-            });
-        });
-    }
-
-});
