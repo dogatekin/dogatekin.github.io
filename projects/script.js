742d07109b0e451065ab6a2895c5b9fc2ceb7548
Doga Tekin <doga.tekin@epfl.ch>
2021-01-02 00:24:56 +0100

    Collapsible projects

---
+++ projects/script.js
@@ -0,0 +1,13 @@
+const carets = document.querySelectorAll('.caret')
+const categories = document.querySelectorAll('.category')
+
+for (const [index, category] of categories.entries()) {
+  category.onclick = function () {
+    if (category.classList.contains('collapsed'))
+      carets[index].style.transform = 'rotate(90deg)'
+    else
+      carets[index].style.transform = 'rotate(0deg)'
+    category.classList.toggle('collapsed')
+    console.log(category.classList)
+  }
+}
