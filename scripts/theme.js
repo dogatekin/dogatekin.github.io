ddb4ef5addb2ce76727447e8bec1a682bc7f502d
Doga Tekin <doga.tekin@epfl.ch>
2021-01-01 23:12:15 +0100

    Change default to dark mode

--- scripts/theme.js
+++ scripts/theme.js
@@ -4,14 +4,14 @@ const currentTheme = localStorage.getItem("theme");
 const lightLogos = document.querySelectorAll(".light-logo")
 const darkLogos = document.querySelectorAll(".dark-logo")
 
-// If the current theme in localStorage is "dark"...
-if (currentTheme == "dark") {
-  // ...then use the .dark-theme class
-  document.body.classList.add("dark-theme");
-  icons.forEach(icon => icon.classList.remove("fa-sun"))
-  icons.forEach(icon => icon.classList.add("fa-moon"))
-  lightLogos.forEach(x => x.classList.add("d-none"))
-  darkLogos.forEach(x => x.classList.remove("d-none"))
+// If the current theme in localStorage is "light"...
+if (currentTheme == "light") {
+  // ...then use the .light-theme class
+  document.body.classList.add("light-theme");
+  icons.forEach(icon => icon.classList.add("fa-sun"))
+  icons.forEach(icon => icon.classList.remove("fa-moon"))
+  lightLogos.forEach(x => x.classList.remove("d-none"))
+  darkLogos.forEach(x => x.classList.add("d-none"))
 }
 
 // Listen for a click on the button 
@@ -19,19 +19,19 @@ btns.forEach(btn => btn.addEventListener("click", function () {
   // Remove focus
   btn.blur();
 
-  // Toggle the .dark-theme class on each click
-  document.body.classList.toggle("dark-theme");
+  // Toggle the .light-theme class on each click
+  document.body.classList.toggle("light-theme");
   icons.forEach(icon => icon.classList.toggle("fa-sun"))
   icons.forEach(icon => icon.classList.toggle("fa-moon"))
   lightLogos.forEach(x => x.classList.toggle("d-none"))
   darkLogos.forEach(x => x.classList.toggle("d-none"))
 
   // Let's say the theme is equal to light
-  let theme = "light";
-  // If the body contains the .dark-theme class...
-  if (document.body.classList.contains("dark-theme")) {
-    // ...then let's make the theme dark
-    theme = "dark";
+  let theme = "dark";
+  // If the body contains the .light-theme class...
+  if (document.body.classList.contains("light-theme")) {
+    // ...then let's make the theme light
+    theme = "light";
   }
   // Then save the choice in localStorage
   localStorage.setItem("theme", theme);
