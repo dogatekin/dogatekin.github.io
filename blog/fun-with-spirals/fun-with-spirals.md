efd629c4ac3639aeb82344094570576fdfc4a281
Doga Tekin <doga.tekin@epfl.ch>
2021-04-28 22:43:03 +0200

    Add some code for spirals blog post

---
+++ blog/fun-with-spirals/fun-with-spirals.md
@@ -0,0 +1,9 @@
+# Fun With Spirals
+
+Recently, I was looking for a way to add a spiral to my website. 
+
+<div id="cover">
+Hello, this is a div.
+
+It works.
+</div>
---
+++ blog/fun-with-spirals/sketches/archimedean.js
@@ -0,0 +1,33 @@
+let archimedean = new p5(function(p) {
+  p.setup = function() {
+    const parentDiv = p.canvas.parentElement
+    p.createCanvas(p.min(parentDiv.offsetWidth, 640), 360)
+    angle = 0
+  }
+
+  p.draw = function() {
+    p.background(180)
+    p.noFill()
+
+    size = 100
+    spiralWidth = 1
+    angle += 0.001
+
+    p.drawSpiral()
+  }
+
+  p.drawSpiral = function() {
+    oldX = p.width / 2;
+    oldY = p.height / 2;
+  
+    for (let i = 0; i < size; i++) {
+      newAngle = angle * i;
+      x = (p.width / 2) + (spiralWidth * newAngle) * Math.sin(newAngle);
+      y = (p.height / 2) + (spiralWidth * newAngle) * Math.cos(newAngle);
+  
+      p.line(oldX, oldY, x, y);
+      oldX = x;
+      oldY = y;
+    }
+  }
+}, "cover")
