53e27234e3993520cb4a9056955b6feb1fae8522
Doga Tekin <doga.tekin@epfl.ch>
2023-04-29 12:51:35 +0200

    Bookshelf server-side loading test

---
+++ src/routes/fun/bookshelf/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let data;
+</script>
+
+<svelte:head>
+	<title>Doga Tekin | Bookshelf</title>
+</svelte:head>
+
+<h1>Bookshelf</h1>
+
+{#each data.bookshelf as { year, books }}
+	<h2>{year}</h2>
+	<ul>
+		{#each books as { title, author, image }}
+			<li>{title} - {author}</li>
+		{/each}
+	</ul>
+{/each}
