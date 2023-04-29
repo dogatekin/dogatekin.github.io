e991b16c1451e75e7ab2120a7234448036124437
Doga Tekin <doga.tekin@epfl.ch>
2023-04-29 13:15:31 +0200

    Update typed messages

--- src/lib/Typed.svelte
+++ src/lib/Typed.svelte
@@ -21,15 +21,14 @@
 	<div id="typed-strings" style="display: none;">
 		<p>I am a computer scientist.</p>
 		<p>I did a Master's at <a href="https://www.epfl.ch/en/">EPFL</a>.</p>
-		<p>I am interested in artificial intelligence.</p>
 		<p>I am a research engineer at <a href="https://labs.oracle.com/">Oracle Labs</a>.</p>
-		<p>I love Python.</p>
+		<p>I am interested in <a href="{base}/blog/makemore-jax/">artificial intelligence</a>.</p>
 		<p>I can juggle <s>2</s> <s>3</s> 4 balls.</p>
 		<p>I have been playing the piano for a year.</p>
 		<p>I like making <a href="{base}/art">animated loops</a>.</p>
-		<p>I am fascinated by <a href="{base}/blog">game design</a>.</p>
+		<p>I am fascinated by <a href="{base}/blog/game-frustration/">game design</a>.</p>
 		<p>I enjoy <a href="{base}/blog/hypnotic-degenerate-spirals/">creative coding</a>.</p>
-		<p>I wish you a pleasant day.</p>
+		<p>I hope you have a great day!</p>
 	</div>
 	<span id="typed" />
 </div>
