2da58e7565bf6b32cc8f6b246a2ca21ad36b9014
Doga Tekin <doga.tekin@epfl.ch>
2023-04-29 13:50:04 +0200

    Updates

--- _app/immutable/chunks/11-9e983223.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/fun/_page.svelte-6040904b.js";export{t as component};
---
+++ _app/immutable/chunks/11-c333387d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/fun/_page.svelte-82710db9.js";export{t as component};
