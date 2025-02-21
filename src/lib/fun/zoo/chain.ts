ecd6ad9ada34ceadb8375a7bd7fb0f4f6ca45aca
Doga Tekin <dotekin@gmail.com>
2025-02-21 19:59:07 +0100

    Add angle constraints

--- src/lib/fun/zoo/chain.ts
+++ src/lib/fun/zoo/chain.ts
@@ -24,9 +24,11 @@ const chain = function (p: p5) {
       let lengths = Array(9).fill(40);
       // let sizes = Array(10).fill().map(() => p.random(20, 60));
       // let lengths = Array(9).fill().map(() => p.random(20, 30));
+      sizes = [68, 75, 81, 84, 83, 77, 64, 51, 38, 32, 14, 4];
+      lengths = Array(sizes.length - 1).fill(40);
       
       chains = [
-        new Chain(sizes, lengths, p.createVector(p.width/2, p.height/2)),
+        new Chain(sizes, lengths, p.createVector(p.width/2, p.height/2), p.PI/6),
         // new Chain(Array(10).fill(30), Array(9).fill(30), p.createVector(50, 50)),
         // new Chain(Array(10).fill(50), Array(9).fill(50), p.createVector(900, 900))
       ];
@@ -40,25 +42,60 @@ const chain = function (p: p5) {
     
     class Link {
       pos: p5.Vector;
+      dir: p5.Vector;
       r: number;
       target?: Link;
       length?: number;
+      constraint?: number;
     
       constructor(x: number, y: number, r: number) {
         this.pos = p.createVector(x, y)
         this.r = r;
       }
 
-      attach(target: Link, length: number) {
+      attach(target: Link, length: number, constraint: number) {
         this.target = target;
         this.length = length;
+        this.constraint = constraint;
       }
     
       update() {
-        if (typeof this.target !== 'undefined' && typeof this.length !== 'undefined') {
-          let dir = p.Vector.sub(this.pos, this.target.pos);
-          this.pos = p.Vector.add(this.target.pos, dir.setMag(this.length));
+        if (typeof this.target !== 'undefined' && typeof this.length !== 'undefined' && typeof this.constraint !== 'undefined') {
+          this.dir = p.Vector.sub(this.target.pos, this.pos);
+          
+          // let angle = this.dir.angleBetween(this.target.dir);
+          // if (angle > this.constraint) {
+          //   angle = this.constraint;
+          // }
+          // else if (angle < -this.constraint) {
+          //   angle = -this.constraint;
+          // }
+
+          let angle = this.dir.heading();
+          let anchor = this.target.dir.heading();
+          angle = this.constrain(angle, anchor, this.constraint);
+
+          this.pos = p.Vector.sub(this.target.pos, p.Vector.fromAngle(angle).setMag(this.length))
+          // this.pos = p.Vector.add(this.target.pos, p.Vector.setMag(this.dir, this.length));
         }
+      }
+
+      constrain(angle: number, anchor: number, constraint: number) {
+        let angleDiff = p.PI - this.simplify(angle + p.PI - anchor);
+
+        if (p.abs(angleDiff) <= constraint) {
+          return this.simplify(angle);
+        }
+
+        if (angleDiff > 0) {
+          return this.simplify(anchor - constraint);
+        }
+
+        return this.simplify(anchor + constraint);
+      }
+
+      simplify(angle: number) {
+        return ((angle % p.TAU) + p.TAU) % p.TAU;
       }
     
       display() {
@@ -74,7 +111,7 @@ const chain = function (p: p5) {
       seed: number;
       heading: p5.Vector;
 
-      constructor(sizes: number[], lengths: number[], pos: p5.Vector) {
+      constructor(sizes: number[], lengths: number[], pos: p5.Vector, constraint: number) {
         this.seed = p.int(p.random(0, 1000));
         this.heading = p.Vector.random2D();
 
@@ -84,13 +121,11 @@ const chain = function (p: p5) {
           let prev = this.links[i-1];
           let pos = p.Vector.sub(prev.pos, this.heading.setMag(lengths[i-1]))
           this.links.push(new Link(pos.x, pos.y, sizes[i]));
-          this.links[i].attach(prev, lengths[i-1]);
+          this.links[i].attach(prev, lengths[i-1], constraint);
         }
-
-        console.log(p);
       }
 
-      screenwrap(l: number = 0, r: number = p.width, u: number = 0, d: number = p.height, pad: number = 20) {
+      screenwrap(l: number = 0, r: number = p.width, u: number = 0, d: number = p.height, pad: number = 50) {
         if (this.links.every(link => link.pos.x > r + pad)) {
           let furthest = Math.max(...this.links.map(link => link.pos.x));
           this.links.forEach(link => link.pos.x -= furthest - l + pad/2);
@@ -122,83 +157,83 @@ const chain = function (p: p5) {
           this.heading.rotate(0.1 * p.map(p.noise(0.05 * p.frameCount), 0, 1, -1, 1));
         }
 
+        this.links[0].dir = this.heading;
         this.links[0].pos.add(p.Vector.setMag(this.heading, speed));
         this.links.forEach(link => link.update());
       }
 
       display() {
-        // this.links.forEach(link => link.display());
-
-        // p.stroke('red');
         p.stroke('#a8d1d1');
         p.fill('#a8d1d1');
-
         p.beginShape();
         
         let head = this.links[0];
 
         // Top of head
         let dir = this.heading;
-        let pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        let pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
         p.curveVertex(pos.x, pos.y);
         
         // Right of head
         dir = p.Vector.rotate(this.heading, p.PI/4);
-        pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
 
         dir = p.Vector.rotate(this.heading, p.PI/2);
-        pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
           
         // Right side of body
         for (let i = 1; i < this.links.length; i++) {
           let cur = this.links[i];
           let dir = p.Vector.sub(cur.target?.pos, cur.pos).rotate(p.PI/2);
-          let pos = p.Vector.add(cur.pos, dir.setMag(cur.r/2));
+          let pos = p.Vector.add(cur.pos, p.Vector.setMag(dir, cur.r/2));
           p.curveVertex(pos.x, pos.y);
         }
         
         // Tail
         let tail = this.links.at(-1);
         dir = p.Vector.sub(tail?.target?.pos, tail?.pos).rotate(3*p.PI/4);
-        pos = p.Vector.add(tail?.pos, dir.setMag(tail.r / 2))
+        pos = p.Vector.add(tail?.pos, p.Vector.setMag(dir, tail.r / 2))
         p.curveVertex(pos.x, pos.y);
         
         dir = p.Vector.sub(tail?.target?.pos, tail?.pos).rotate(p.PI);
-        pos = p.Vector.add(tail?.pos, dir.setMag(tail.r / 2))
+        pos = p.Vector.add(tail?.pos, p.Vector.setMag(dir, tail.r / 2))
         p.curveVertex(pos.x, pos.y);
 
         dir = p.Vector.sub(tail?.target?.pos, tail?.pos).rotate(-3*p.PI/4);
-        pos = p.Vector.add(tail?.pos, dir.setMag(tail.r / 2))
+        pos = p.Vector.add(tail?.pos, p.Vector.setMag(dir, tail.r / 2))
         p.curveVertex(pos.x, pos.y);
 
         // Left side of body
         for (let i = this.links.length - 1; i > 0; i--) {
           let cur = this.links[i];
           let dir = p.Vector.sub(cur.target?.pos, cur.pos).rotate(-p.PI/2);
-          let pos = p.Vector.add(cur.pos, dir.setMag(cur.r/2));
+          let pos = p.Vector.add(cur.pos, p.Vector.setMag(dir, cur.r/2));
           p.curveVertex(pos.x, pos.y);
         }
 
         // Left of head
         dir = p.Vector.rotate(this.heading, -p.PI/2);
-        pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
         
         dir = p.Vector.rotate(this.heading, -p.PI/4);
-        pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
 
         // Back to the top of head
         dir = this.heading;
-        pos = p.Vector.add(head.pos, dir.setMag(head.r/2));
+        pos = p.Vector.add(head.pos, p.Vector.setMag(dir, head.r/2));
         p.curveVertex(pos.x, pos.y);
         p.curveVertex(pos.x, pos.y);
         
         p.endShape();
+
         p.stroke(255);
+        p.noFill();
+        this.links.forEach(link => link.display());
       }
     }
 }
--- src/routes/fun/zoo/+page.svelte
+++ src/routes/fun/zoo/+page.svelte
@@ -10,3 +10,12 @@
 <div class="content">
 	<P5 sketch={chain} />
 </div>
+
+<!-- 
+Resources for procedural animations:
+- https://www.youtube.com/watch?v=qlfh_rv6khY
+- https://weaverdev.io/projects/bonehead-procedural-animation/
+- https://www.alanzucconi.com/2017/04/17/procedural-animations/
+- https://zalo.github.io/blog/constraints/
+- https://x.com/TheRujiK/status/969581641680195585
+-->
